8aa742d07ead2ab960013e8480b787c8b53101f2
hapso <lemauxcharles@yahoo.com>
2023-12-27 14:02:16 +0100

    [+] New keyboard listener

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
-includes= includes/gl.h\
-					-I/usr/include/vulkan/\
+includes= 	includes/gl.h\
+			-I/usr/include/vulkan/\
+			-I/usr/include/libevdev-1.0
 
-SRC=	src/main.c\
-		src/window.c\
+SRC=		src/main.c\
+			src/window.c\
 
-LINKS=  -lvulkan -lcsfml-graphics -lcsfml-window -lcsfml-system -l    csfml-audio -lcsfml-network
+LINKS=  -lvulkan -lcsfml-graphics -lcsfml-window -lcsfml-system -lcsfml-audio -lcsfml-network -levdev
 
 all: clean build
 
--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include "../includes/gl.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/input.h>
+#include <string.h>
+#include <libevdev/libevdev.h>
 
 static Options opts;
 
@@ -48,6 +53,49 @@ static void event_handler(sfRenderWindow* w, sfEvent* e)
 
 int main(int argc, char** argv)
 {
+    int i = 1;
+    struct libevdev *dev = NULL;
+    int fd;
+    int rc;
+
+    fd = open("/dev/input/event3", O_RDONLY|O_NONBLOCK); //TODO : Keyboard listener (from /dev/input/by-id)
+    if (fd < 0) {
+        perror("Failed to open the input device");
+        exit(EXIT_FAILURE);
+    }
+
+    rc = libevdev_new_from_fd(fd, &dev);
+    if (rc < 0) {
+        perror("Failed to init libevdev");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    while(i) {
+        struct input_event ev;
+        rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
+        if (rc == 0 && ev.type == EV_KEY && ev.value == 1) {
+            switch (ev.code) {
+                case KEY_SPACE:
+                    printf("Spacebar pressed\n");
+                    break;
+                case KEY_X:
+                    printf("X pressed\n");
+                    break;
+                case KEY_TAB:
+                    i = 0;
+                    break;
+            }
+        }
+    }
+
+    libevdev_free(dev);
+    close(fd);
+    exit(EXIT_SUCCESS);
+}
+
+__attribute__((constructor)) void init()
+{
 	sfVideoMode mode = {800, 600, 32};
 	sfRenderWindow *window = VK_NULL_HANDLE;
 	sfEvent event;
@@ -62,5 +110,4 @@ int main(int argc, char** argv)
 		window_manager(window);
 	}
 	sfRenderWindow_destroy(window);
-    exit(EXIT_SUCCESS);
 }
