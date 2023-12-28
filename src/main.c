b906d46dd4031d3a24f8e6837774e38271a5a2cd
hapso <lemauxcharles@yahoo.com>
2023-12-28 03:45:22 +0100

    [+] Syspath : retrieve input path [!] TODOS

--- src/main.c
+++ src/main.c
@@ -89,17 +89,49 @@ int main(int argc, char** argv)
             }
         }
     }
-
     libevdev_free(dev);
     close(fd);
+    printf("\nEXIT_SUCCESS\n");
     exit(EXIT_SUCCESS);
+}
+
+static int extractInputNumber(const char *syspath)
+{
+    const char *inputStr = "input";
+    char *lastInput = strrchr(syspath, '/');
+    if (lastInput != NULL) {
+        lastInput = strstr(lastInput, inputStr);
+        if (lastInput != NULL) {
+            lastInput += strlen(inputStr);
+            return atoi(lastInput);
+        }
+    }
+    return (0);
+}
+
+static int is_keyboard_valid(const char *c) //TODO : refactor this function
+{
+    char full_path[256];
+    int event_nb = extractInputNumber(c);
+    snprintf(full_path, sizeof(full_path), "%s%d\n", "/dev/input/event", event_nb);
+
+    printf("%s\n", full_path);
+    FILE *file = fopen(full_path, "r");
+    if (file){
+        printf("VALID\n");
+        fclose(file);
+        return (1);
+    } else {
+        printf("INVALID\n");
+        return (0);
+    }
 }
 
 static int get_keyboards() {
     struct udev *udev = udev_new();
     if (!udev) {
         printf("Failed to create udev\n");
-        return 1;
+        return (1);
     }
 
     struct udev_enumerate *enumerate = udev_enumerate_new(udev);
@@ -113,22 +145,27 @@ static int get_keyboards() {
         const char *path = udev_list_entry_get_name(entry);
         struct udev_device *dev = udev_device_new_from_syspath(udev, path);
         const char *devtype = udev_device_get_property_value(dev, "ID_INPUT_KEYBOARD");
-        if (devtype != NULL && strcmp(devtype, "1") == 0) {
-            printf("Keyboard device: %s\n", udev_device_get_syspath(dev));
-            printf("Keyboard name: %s\n", udev_device_get_property_value(dev, "NAME"));
-            // Store the keyboard device information as needed
+        const char *name = udev_device_get_property_value(dev, "NAME");
+        if (devtype != NULL && strcmp(devtype, "1") == 0 && name != NULL) {
+            //printf("Keyboard device: %s\n", udev_device_get_syspath(dev));
+            //printf("Keyboard name: %s\n", udev_device_get_property_value(dev, "NAME"));
+            if (is_keyboard_valid(udev_device_get_syspath(dev)))
+                printf("VALID\n");
+            // TODO : Store the keyboard device information as needed
         }
         udev_device_unref(dev);
     }
-
     udev_enumerate_unref(enumerate);
     udev_unref(udev);
 
-    return 0;
+    return (0);
 }
 
-__attribute__((constructor)) void init()
+__attribute__((constructor)) int init(int argc)
 {
+    if (argc > 1) {
+        return (0);
+    }
     get_keyboards();
 	sfVideoMode mode = {800, 600, 32};
 	sfRenderWindow *window = VK_NULL_HANDLE;
@@ -144,4 +181,5 @@ __attribute__((constructor)) void init()
 		window_manager(window);
 	}
 	sfRenderWindow_destroy(window);
+    return (1);
 }
